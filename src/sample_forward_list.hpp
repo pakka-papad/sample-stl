67f5367a5227e89c016a316dae6506727f1f48c3
pakka_papad <76241334+pakka-papad@users.noreply.github.com>
2024-11-04 20:59:10 +0530

    Added empty, clear, push_front and pop_front functions

--- src/sample_forward_list.hpp
+++ src/sample_forward_list.hpp
@@ -1,12 +1,42 @@
 #ifndef SAMPLE_FORWARD_LIST_H
 #define SAMPLE_FORWARD_LIST_H
 
+#include <stdexcept>
+
 namespace sample {
-    
+
     template<typename T>
     class forward_list {
         public:
         forward_list();
+
+        bool empty() const noexcept;
+
+        void push_front(const T& value);
+
+        void pop_front();
+
+        void clear();
+
+        class iterator {
+            public:
+            iterator& operator++();
+
+            iterator operator++(int);
+
+            T& operator*();
+
+            bool operator==(const iterator &other) const noexcept;
+
+            bool operator!=(const iterator &other) const noexcept;
+
+            T* operator->() const noexcept;
+            
+            iterator(T* ptr);
+
+            private:
+            T* ptr = nullptr;
+        };
 
         private:
         class node {
@@ -20,6 +50,7 @@ namespace sample {
         };
 
         node* _head;
+        size_t _size;
     };
 
     template<typename T>
@@ -28,15 +59,46 @@ namespace sample {
     }
 
     template<typename T>
-    forward_list<T>::node::node(const T& item) {
-        this->item = item;
-        this->next = nullptr;
+    forward_list<T>::node::node(const T& item): item(item), next(nullptr) {
     }
 
     template<typename T>
-    forward_list<T>::node::node(const T& item, node* next) {
-        this->item = item;
-        this->next = next;
+    forward_list<T>::node::node(const T& item, node* next): item(item), next(next) {
+    }
+
+    template<typename T>
+    bool forward_list<T>::empty() const noexcept {
+        return (_size != 0);
+    }
+
+    template<typename T>
+    void forward_list<T>::push_front(const T& value) {
+        node* n = new node(value);
+        n->next = _head;
+        _head = n;
+        _size++;
+    }
+
+    template<typename T>
+    void forward_list<T>::pop_front() {
+        if (_head == nullptr) {
+            throw std::runtime_error("pop_front() called on list of size 0.");
+        }
+        node* n = _head;
+        _head = _head->next;
+        delete(n);
+        _size--;
+    }
+
+    template<typename T>
+    void forward_list<T>::clear() {
+        node* p = _head;
+        while (p != nullptr) {
+            node* n = p;
+            p = p->next;
+            delete(n);
+            _size--;
+        }
     }
 }
 
--- src/test_forward_list.cpp
+++ src/test_forward_list.cpp
@@ -1,9 +1,44 @@
 #include "sample_forward_list.hpp"
 #include <iostream>
 
+int t = 1;
+
+class A {
+    public:
+    int x;
+    A() {
+        x = t;
+        t++;
+        std::cout << "A constructor called " << x << '\n';
+    }
+    ~A() {
+        std::cout << "A destructor called " << x << '\n';
+    }
+    A(const A& other) {
+        this->x = other.x;
+        std::cout << "A copy constructor called " << x << '\n';
+    }
+    A& operator=(const A& other) {
+        if (this != &other) {
+            this->x = other.x;
+            std::cout << "A assignment operator called " << x << '\n';
+        }
+        return *this;
+    }
+    void print(){
+        std::cout << "A print called " << x << '\n';
+    }
+};
+
 int main() {
-    sample::forward_list<int> l;
-    
+    sample::forward_list<A> l;
+    for (int i = 0; i < 5; i++) {
+        std::cout << "\tPushing " << t << "\n";
+        l.push_front(A());
+        std::cout << "\tPushed " << t-1 << "\n";
+    }
+    l.pop_front();
+    l.clear();
     return EXIT_SUCCESS;
 }
 
